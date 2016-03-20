9c7000062e370a637eab428b74812757d5d81699
Sam Brannon <sam@branberg.com>
2016-03-20 17:06:58 -0500

    Used previously unused avatar helper. Doh.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def profile_avatar_select(user)
-    return image_tag user.avatar.url(:medium), id: 'image-preview', class: 'avatar' if user.avatar.exists?
-    image_tag 'default-avatar.png', id: 'image-preview', class: 'avatar'
+    return image_tag user.avatar.url(:medium), class: 'profile__avatar' if user.avatar.exists?
+    image_tag 'default-avatar.png', class: 'profile__avatar'
   end
 end
