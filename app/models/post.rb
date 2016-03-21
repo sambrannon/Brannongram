9d89460e7f94373debc247192f7eea9023a8f5a6
Sam Brannon <sam@sambrannon.com>
2016-03-21 14:19:37 -0500

    Changed min post length to 1, gotta love emoji. Added js to toggle like heart of success.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :image, presence: true
-  validates :caption, presence: true, length: { minimum: 3, maximum: 300 }
+  validates :caption, presence: true, length: { minimum: 1, maximum: 300 }
 
   has_attached_file :image, styles: { :medium => '640x' }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
